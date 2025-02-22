8e6485c63ea0a0e3604143ff80cea8ea5efbd535
RiyyaduSolihin <riyyadu1234@gmail.com>
2025-02-22 14:42:42 +0700

    mengerjakan tugas pertemuan 3

---
+++ Pertemuan3/ListMain.java
@@ -0,0 +1,19 @@
+
+package Pertemuan3;
+
+public class ListMain {
+	
+	public static void main(String[] args) {
+		StrukturList list = new StrukturList();
+		list.addHead(7);
+		list.addHead(5);
+		list.addHead(4);
+		list.addHead(1);
+		
+		System.out.println("Elemen: ");
+		list.displayElement();
+	}
+	
+
+
+}
